94e401f8284d1726236bec31dbee5efdd6f3b329
Luis Regus <luisregus@Luiss-MacBook.home>
2016-07-26 10:22:07 -0400

    adds contact route

--- flask/controllers/general.py
+++ flask/controllers/general.py
@@ -13,10 +13,17 @@ def index():
 @app.route('/resume')
 def resume_page():
     page_title = 'Resume'
-    return 'resume page'
+    slug = 'resume'
+    return render_template('pages/resume.html', slug = slug, page_title = page_title)
 
 @app.route('/code-snippets')
 def code_snippets():
     page_title = 'Code Snippets'
     slug = 'code-snippets'
     return render_template('pages/code-snippets.html', slug = slug, page_title = page_title)
+
+@app.route('/contact')
+def contact():
+    page_title = 'Contact Me'
+    slug = 'contact'
+    return render_template('pages/contact.html', slug = slug, page_title = page_title)
