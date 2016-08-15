afd6422f7ef0fb16fe793316a7eab53adaae9ab9
Luis Regus <luisregus@Luiss-MacBook.local>
2016-08-15 15:23:03 -0400

    moving

---
+++ app.py
@@ -0,0 +1,11 @@
+import os
+from server import app
+
+from controllers import general
+from controllers import code_snippet
+
+if __name__ == '__main__':
+    app.run(
+        host = '0.0.0.0',
+        port = 3733
+    )
